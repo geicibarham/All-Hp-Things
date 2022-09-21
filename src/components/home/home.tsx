43a2b039a5dccfa8310d2279cbe75c0bb610493b
geici <geicianecosta43@gmail.com>
2022-09-20 22:46:23 -0400

    worked on data and style today

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,19 +1,18 @@
 import styles from "./home.module.css";
 import "animate.css";
 import Footer from "../footer/footer";
+import { Link } from 'react-router-dom'
 const Home = () => {
-  const test = () => {
-    console.log("test");
-  };
+
   return (
     <>
       <section id={styles.home}>
-        <button
-          className="animate__animated animate__backInDown"
-          onClick={test}
-        >
+
+      <Link to="/characters">
+        <button>
           Enter
         </button>
+        </Link>
       </section>
 
       <Footer />
